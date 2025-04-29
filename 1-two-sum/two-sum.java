ee5e4f0f4f204c3eaefc970b1f63108365a79a63
Chinmay Gambhirrao <91001957+ChinmayGambhirrao@users.noreply.github.com>
2025-04-29 23:29:09 +0530

    Time: 2 ms (98.83%) | Memory: 45.2 MB (30.69%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,12 +4,11 @@ class Solution {
 
         for(int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-
             if(numMap.containsKey(complement)) {
-                return new int[] {numMap.get(complement), i};
+                return new int[]{numMap.get(complement), i};
             }
             numMap.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
-}
+        return null;
+    }
 }
