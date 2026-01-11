7c4c9079a342597de2bebabd0c7951fb8ffe866a
Chinmay Gambhirrao <91001957+ChinmayGambhirrao@users.noreply.github.com>
2026-01-11 17:05:59 +0530

    Time: 8 ms (88.20%) | Memory: 61.4 MB (6.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,8 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) {
-        return false;
+    if(s.length !== t.length) return false;
+
+    const count = {};
+
+    for(let char of s) {
+        count[char] = (count[char] || 0) + 1;
     }
-    return s.split('').sort().join('') === t.split('').sort().join('');
+
+    for(let char of t) {
+        if(!count[char]) return false;
+        count[char]--;
+    }
+    return true;
 };
